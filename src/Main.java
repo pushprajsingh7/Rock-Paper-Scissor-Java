277a71339c4fd52b085b75e550992fe597281738
Pushpraj <teamsf@blucursor.com>
2023-03-23 19:03:54 +0530

    Flow changed

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,72 @@
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
+	static int count = 0;
+	static int trial=5;
+	static List<String> win=new ArrayList<>();
+	static List<String>lost=new ArrayList<>();
+	static List<String> draw=new ArrayList<>();
 	public static void main(String args[]) {
-		int count = 0;
-		int trial = 5;
+		
+		Program pg = new Program();
+		
+		
 		do {
-			Program pg = new Program();
-
-			pg.logic();
+			
+			Map<Integer, String> options = new LinkedHashMap<>();
+			options.put(0, "Rock");
+			options.put(1, "Paper");
+			options.put(2, "Scissor");
+			options.put(9, "terminate");
+			System.out.println("Please select an option from numbers : "+options);
+			Scanner sc = new Scanner(System.in);
+			int selected = sc.nextInt();
+			System.out.println("You selected :"+selected);
+			Set<Integer> keyset = options.keySet();
+			Random random = new Random();
+			String Automatic = options.get(random.nextInt(keyset.size() - 1));
+			
+			
+			if (selected == 0) {
+				System.out.println("Selected option is 0: Rock ");
+			} else if (selected == 1) {
+				System.out.println("Selected option is 2: Paper ");
+			} else if (selected == 2) {
+				System.out.println("Selected option is 2: Scissor ");
+			}
+			else if(selected==9) {
+				System.out.println("Terminated :No Analysis");
+				analysis();
+				break;
+			}
+			Boolean flag = options.containsKey(selected);
+			try {
+				pg.logic(win,selected,flag,Automatic,options,lost,draw);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			count++;
 			trial--;
-			System.out.println("Played times is :" + count);
-			System.out.println("Free trial remaining is :" + trial);
+			
 		} while (trial != 0);
 		if (trial == 0) {
-			System.out.println("Sport terminated");
+			analysis();
 		}
+	
+	}
+	public static void analysis() {
+		System.out.println("Played times is :" + count);
+		System.out.println("Free trial remaining is :" + trial);
+		System.out.println("Won times :"+win.size());
+		System.out.println("Lost times  :"+lost.size());
+		System.out.println("No Contest :"+draw.size());
+		System.out.println("Sport terminated");
 	}
 }
--- src/Program.java
+++ src/Program.java
@@ -1,54 +1,48 @@
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Program {
-	void logic() {
+	void logic(List<String> list,int selected,boolean flag,String Automatic,Map<Integer,String>options,List<String>lost,List<String>draw) {
 		String win = "you Win";
 		String lose = "you lose";
-		Map<Integer, String> options = new LinkedHashMap<>();
-		options.put(0, "Rock");
-		options.put(1, "Paper");
-		options.put(2, "Scissor");
-		Set<Integer> keyset = options.keySet();
-		Random random = new Random();
-		String Automatic = options.get(random.nextInt(keyset.size() - 1));
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Select your option" + options);
-		int selected = sc.nextInt();
-		if (selected == 0) {
-			System.out.println("Selected option is 0: Rock ");
-		} else if (selected == 1) {
-			System.out.println("Selected option is 2: Paper ");
-		} else if (selected == 2) {
-			System.out.println("Selected option is 2: Scissor ");
-		}
-		Boolean flag = options.containsKey(selected);
+		
 		if (!(flag)) {
 			System.out.println("enter any number from  displayed options: options are :" + options);
 		} else {
 			System.out.println("Automatic  is : " + Automatic);
 			if (Automatic == "Rock" && selected == 1) {
 				System.out.println(win);
+				list.add(win);
+				
 				
 			} else if (Automatic == "Paper" && selected == 2) {
 				System.out.println(win);
+				list.add(win);
+
 				
 			} else if (Automatic == "Scissor" && selected == 0) {
 				System.out.println(win);
+				list.add(win);
+
 				
 			} else if (selected == 0 && Automatic == "Paper") {
 				System.out.println(lose);
+				lost.add(lose);
 				
 			} else if (selected == 1 && Automatic == "Scissor") {
 				System.out.println(lose);
+				lost.add(lose);
 				
 			} else if (selected == 2 && Automatic == "Rock") {
 				System.out.println(lose);
+				lost.add(lose);
 				
 			} else {
+				draw.add("draw");
 				System.out.println("Game Draw");
 				
 			}
